aafef88782fe32001c6787cdc1c0b5621ee354fb
Rasib0 <rasibnadeem101@gmail.com>
2023-11-11 19:44:30 +0500

    Im done for now

--- src/generics/bounds.rs
+++ src/generics/bounds.rs
@@ -4,6 +4,53 @@ fn printer<T: Display>(t: T) {
     println!("{}", t);
 }
 
+struct S<T: Display>(T);
+
+use std::fmt::Debug;
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 {
+        self.length * self.height
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: f64,
+    height: f64,
+}
+#[allow(dead_code)]
+struct Triangle {
+    length: f64,
+    height: f64,
+}
+
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+fn area<T: HasArea>(t: &T) -> f64 {
+    t.area()
+}
+
 pub(crate) fn main() {
-    todo!()
+    let rectangle = Rectangle {
+        length: 3.0,
+        height: 4.0,
+    };
+
+    let _triangle = Triangle {
+        length: 3.0,
+        height: 4.0,
+    };
+
+    print_debug(&rectangle);
+    println!("Area: {}", area(&rectangle));
+
+    // print_debug(&_triangle)
+    println!("Area: {}", area(&_triangle));
 }
